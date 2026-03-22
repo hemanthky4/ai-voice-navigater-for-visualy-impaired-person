84cd03e7977aeabb64575e3454903606476c3d71
Hemanth <hemanthky4@github.com>
2026-03-22 20:59:48 +0530

    Update BACKEND_URL to Render deployment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let BACKEND_URL = 'http://127.0.0.1:5000';
+    let BACKEND_URL = 'https://ai-voice-navigater-for-visualy-impaired.onrender.com';
 
 
     // ---- AI Voice Feedback Helper ----
